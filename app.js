7e55422be92873a74cbc7c509d0c028886308058
jcarreazo <jcarreazos.dev@gmail.com>
2021-07-29 20:49:22 -0500

    Adding menu mobile animations

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-let toggleSwitch=document.getElementById("switch");
+let toggleSwitch = document.getElementById("switch");
+let menuButton = document.getElementById("menu");
+let navBar = document.getElementById("navbar");
+navBar.style.display = ("none")
+navBar.hidden=true;
+let navBarStatus = navBar.hidden;
 
-toggleSwitch.addEventListener("click",()=>{
-
+menuButton.addEventListener("click", () => {
+    navBarStatus = !navBarStatus;
+    console.log(navBarStatus);
+    toggleMenu();
+    
 })
+
+function toggleMenu() {
+    if (navBarStatus == true) {
+        navBar.style.display = ("none");
+    } else {
+        navBar.style.animation("menu-move ease-in 0.5s;");
+        navBar.style.display = ("flex");
+        
+    }
+}
